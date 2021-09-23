2de9055e10aa58deb48cea1b6bb37c2cc2abc28c
ruvan-der <ruvan-der@student.wethinkcode.co.za>
2021-09-23 11:27:34 +0200

    added my aliases

---
+++ env_scripts/setup.sh
@@ -0,0 +1,15 @@
+alias t='python3 -m unittest tests/test_main.py -v -b'
+# soft test
+alias st='python3 -m unittest tests/test_main.py'
+alias p='python3'
+alias c='clear'
+alias w='wtc-lms'
+alias wl='wtc-lms login'
+alias reloadalias='source ~/.zshrc'
+alias editalias='code ~/.zshrc'
+alias catalias='cat ~/.zshrc'
+alias idea='sh /home/bin/idea-IC-203.7148.57/bin/idea.sh'
+export PATH="$PATH:$HOME/bin"
+export PATH=/bin/apache-maven-3.6.3/bin:$PATH
+alias gc='git checkout'
+alias gcm='git checkout main'
